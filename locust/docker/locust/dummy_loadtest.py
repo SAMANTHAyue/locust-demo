bd5a7779d34fc504d090f0a739e9d222bd61eeb0
SAMANTHAyue <zhangzhang.yue@duke.edu>
2020-07-29 08:21:08 -0400

    initial commit to this branch

--- locust/docker/locust/dummy_loadtest.py
+++ locust/docker/locust/dummy_loadtest.py
@@ -1,15 +1,18 @@
-import os
+import uuid
 import time
 import logging
 from locust import HttpLocust, TaskSet, task
 
 logger = logging.getLogger(__name__)
-logger.setLevel(os.getenv("LOCUST_LOG_LEVEL", "INFO").upper())
 
-class DummyTaskSet(TaskSet):
+class MetricsTaskSet(TaskSet):
+    _deviceid = None
 
-    @task
-    def get_dummy_datetime(self):
+    def on_start(self):
+        self._deviceid = str(uuid.uuid4())
+
+    @task(1)
+    def login(self):
         time_start = time.time()
         response = self.client.get("/datetime")
         time_end = time.time()
@@ -19,9 +22,5 @@ class DummyTaskSet(TaskSet):
                                                  duration=round(time_end - time_start, 3)))
 
 
-class DummyLoadTester(HttpLocust):
-    host = os.getenv("LOCUST_TARGET_HOST", "localhost:8080")
-    task_set = DummyTaskSet
-    min_wait = 5000
-    max_wait = 15000
-
+class MetricsLocust(HttpLocust):
+    task_set = MetricsTaskSet
